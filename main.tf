a32ea526982dcb29ca1cebc3f68a415911b888a1
PrasannaNaikAP <74963249+PrasannaNaikAP@users.noreply.github.com>
2024-07-28 23:41:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,32 @@
-aAxAXA
+provider "aws" {
+  alias = "us-east-1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias = "us-east-2"
+  region = "us-east-2"
+}
+
+module "instance_1" {
+  source = "/ec2_instance"
+  providers = {
+    aws = aws.us-east-1
+  }
+  vpc_cidr_block = "10.0.0.0/16"
+  subnet_cidr_block = "10.0.1.0/24"
+  ami_id = "ami-08a0d1e16fc3f61ea"
+
+}
+
+module "instance_2" {
+  source = "/ec2_instance"
+  providers = {
+    aws = aws.us-east-2
+  }
+  vpc_cidr_block = "192.168.0.0/16"
+  subnet_cidr_block = "192.168.1.0/24"
+  ami_id = "ami-033fabdd332044f06"
+
+}
+
